05d59f3c5d06dace965accb23bb87cb4ce853616
tanyaleepr <tanyaleepr@gmail.com>
2022-04-17 21:43:48 -0500

    about me done, but contacts needs repairs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Nav from './components/Nav';
+import About from './components/About';
 
 function App() {
   return (
     <div>
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
     </div>
+
+const categories = [
+  {
+    name: "commercial",
+    description:
+      "Photos of grocery stores, food trucks, and other commercial projects",
+  },
+  { name: "portraits", description: "Portraits of people in my life" },
+  { name: "food", description: "Delicious delicacies" },
+  {
+    name: "landscape",
+    description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+  },
+];
   );
 }
 
---
+++ src/components/Nav/index.js
@@ -0,0 +1,19 @@
+<header>
+  <h2>
+    <a href="/">
+      <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+    </a>
+  </h2>
+  <nav>
+    <ul className="flex-row">
+      <li className="mx-2">
+        <a href="#about">
+          About me
+        </a>
+      </li>
+      <li>
+        <span>Contact</span>
+      </li>
+    </ul>
+  </nav>
+</header>
