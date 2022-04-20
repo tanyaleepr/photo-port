c0b244da83a3ca803fcaf2d93e473288c8fcdba7
tanyaleepr <tanyaleepr@gmail.com>
2022-04-19 19:40:47 -0500

    added components to nav bar and created the test folder/snapshots for about section

--- src/App.js
+++ src/App.js
@@ -10,21 +10,10 @@ function App() {
         <About></About>
       </main>
     </div>
-
-const categories = [
-  {
-    name: "commercial",
-    description:
-      "Photos of grocery stores, food trucks, and other commercial projects",
-  },
-  { name: "portraits", description: "Portraits of people in my life" },
-  { name: "food", description: "Delicious delicacies" },
-  {
-    name: "landscape",
-    description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-  },
-];
-  );
+  )
 }
+
+ 
+
 
 export default App;
